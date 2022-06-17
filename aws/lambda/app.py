f9b43361429bbfcb1899a5477160d7d12b8627d8
Chris Bertinato <cbertinato@ns1.com>
2022-06-17 17:06:06 -0400

    Print out event

--- aws/lambda/app.py
+++ aws/lambda/app.py
@@ -9,6 +9,7 @@ def lambda_handler(event, context):
 
     if (src := event.get('source')) is None or src != 'aws.route53':
         print(f'received out-of-band message')
+        print(event)
         return
     
     detail = event.get('detail')
