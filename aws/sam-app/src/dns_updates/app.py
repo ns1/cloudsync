1388e8643f1a9ce650117c7872f0e5750b4d94db
Chris Bertinato <chris.bertinato@ibm.com>
2024-02-05 11:06:48 -0500

    Add zone omit logic

--- aws/sam-app/src/dns_updates/app.py
+++ aws/sam-app/src/dns_updates/app.py
@@ -9,10 +9,12 @@ from common import retrieve_secret, snapshot_zone
 endpoint = os.environ.get('ENDPOINT')
 zone_omit_enabled = os.environ.get('ENABLE_ZONE_OMIT', True)
 zone_omit_tag = os.environ.get('ZONE_OMIT_TAG', 'CloudSync')
+account_id = os.environ.get('ACCOUNT_ID')
 
 route53_client = boto3.client('route53')
 
-def record_handler(record, endpoint):
+
+def record_handler(record):
     try: 
         body = json.loads(record['body'])
 
@@ -21,8 +23,6 @@ def record_handler(record, endpoint):
         print(record['body'])
         return record['messageId']
     
-    account_id = os.environ.get('ACCOUNT_ID')
-
     # discard changes made by CloudSync-outbound
     user_identity_arn = body['detail']['userIdentity']['arn']
     cloudsync_arn = f"arn:aws:sts::{account_id}:assumed-role/NS1_CloudSync_Role/cloudsync-{account_id}"
@@ -39,18 +39,67 @@ def record_handler(record, endpoint):
         
         case 'CreateHostedZone':
             zone_id = body['detail']['responseElements']['hostedZone']['id'].split('/')[-1]
+            return handle_zones_and_records(zone_id, record)
         
         case 'ChangeResourceRecordSets':
             zone_id = body['detail']['requestParameters']['hostedZoneId'].split('/')[-1]
-        
+            return handle_zones_and_records(zone_id, record)
+
         case 'DeleteHostedZone':
             zone_id = body['detail']['requestParameters']['id']
+            return handle_zones_and_records(zone_id, record)
 
         case 'ChangeTagsForResource':
+            if body['detail']['requestParameters']['resourceType'] != 'hostedzone':
+                # TODO: handle health check tags, which are used for health check names, apparently.
+                return
+            
             zone_id = body['detail']['requestParameters']['resourceId']
+            return handle_zones_and_records(zone_id, record)
+        
+        case 'CreateHealthCheck' | 'DeleteHealthCheck':
+            return handle_health_checks(record)
+    
 
+def handle_health_checks(record):
+    try: 
+        body = json.loads(record['body'])
+
+    except json.JSONDecodeError:
+        print(f"error decoding body as JSON")
+        print(record['body'])
+        return record['messageId']
+    
+
+    msg = {
+        'source': 'AWS-Route53',
+        'version': 1,
+        'account_id': account_id,
+        'auth_key': retrieve_secret(os.environ['SECRET_NAME']),
+        'msg_type': 'update',
+        'zone_name': 'health-checks',
+        'page': 1,
+        'truncated': False,
+        'payload': body['detail']
+    }
+    
+    return send_message(msg, record['messageId'])
+
+        
+def handle_zones_and_records(zone_id, record):
+    try: 
+        body = json.loads(record['body'])
+
+    except json.JSONDecodeError:
+        print(f"error decoding body as JSON")
+        print(record['body'])
+        return record['messageId']
+    
+    event_name = body['detail'].get('eventName')
 
     tags = []
+    zone_name = None
+
     if event_name != 'DeleteHostedZone':
     # TODO: Make zone name optional, because updates don't need it.
     # TODO: Why not pull the zone name from the payload for zone create?
@@ -63,24 +112,23 @@ def record_handler(record, endpoint):
         )
 
         tags = tags['ResourceTagSet'].get('Tags', [])
-        print(tags)
 
-    if zone_omit_enabled:
-        # check whether the zone_omit tag was added
-        if event_name == 'ChangeTagsForResource':
-            new_tags = body['detail']['requestParameters'].get('addTags', [])
+        if zone_omit_enabled:
+            # check whether the zone_omit tag was added
+            if event_name == 'ChangeTagsForResource':
+                new_tags = body['detail']['requestParameters'].get('addTags', [])
 
-            if zone_omit_tag in [t['key'] for t in new_tags]:
-                # the zone_omit tag may have been added. in fact, all tags show up here
-                # regardless of whether they were just added, so we'll have to snapshot
-                # in case the tag was just added. 
-                print("snapshotting")
-                snapshot_zone(route53_client, zone_id, zone_name, account_id, endpoint, tags)
+                if zone_omit_tag in [t['key'] for t in new_tags]:
+                    # the zone_omit tag may have been added. in fact, all tags show up here
+                    # regardless of whether they were just added, so we'll have to snapshot
+                    # in case the tag was just added. 
+                    print("snapshotting")
+                    snapshot_zone(route53_client, zone_id, zone_name, account_id, endpoint, tags)
+                    return
+
+            if zone_omit_tag not in [t['Key'] for t in tags]:
+                # skip zone
                 return
-
-        if zone_omit_tag not in [t['Key'] for t in tags]:
-            # skip zone
-            return
 
     msg = {
         'source': 'AWS-Route53',
@@ -95,6 +143,9 @@ def record_handler(record, endpoint):
         'payload': body['detail']
     }
     
+    return send_message(msg, record['messageId'])
+
+def send_message(msg, message_id):
     json_msg = json.dumps(msg)
     headers = {
         'content-type' : 'application/json',
@@ -106,21 +157,13 @@ def record_handler(record, endpoint):
 
     if response.status_code != 202:
         print(f"POST to {endpoint} failed with {response.status_code}: {response.content}")
-        return record['messageId']
-        
-    return
-
+        return message_id 
 
 def handler(event, context):
-    if (endpoint := os.environ.get('ENDPOINT')) and endpoint is None:
-        print("ENDPOINT env variable not set")
-        # Fail the whole batch
-        return ""
-
     print(event)
     response = {"batchItemFailures": []}
     for record in event.get('Records'):
-        if (failedMessageId := record_handler(record, endpoint)) is not None:
+        if (failedMessageId := record_handler(record)) is not None:
             response['batchItemFailures'].append({"itemIdentifier": failedMessageId})
     
     print(response)
