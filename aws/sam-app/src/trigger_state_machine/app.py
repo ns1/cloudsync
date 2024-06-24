270dfbc9d39bf7a3cd8f100c69357eddfeaee4b9
mnalla-ns1 <159820518+mnalla-ns1@users.noreply.github.com>
2024-06-24 13:00:35 -0400

    unit tests (#3)
    * unit tests
    
    * revert formatted change

--- aws/sam-app/src/trigger_state_machine/app.py
+++ aws/sam-app/src/trigger_state_machine/app.py
@@ -2,28 +2,30 @@ import os
 import boto3
 from crhelper import CfnResource
 
-helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL', sleep_on_delete=120, ssl_verify=None)
-
-try:
-    step_functions_client = boto3.client('stepfunctions')
-    state_machine_arn = os.environ.get('STATE_MACHINE_ARN')
-except Exception as e:
-    helper.init_failure(e)
+helper = CfnResource(json_logging=False, log_level='DEBUG',
+                     boto_level='CRITICAL', sleep_on_delete=120, ssl_verify=None)
 
 
 @helper.create
 def create(event, context):
+    step_functions_client = boto3.client('stepfunctions')
+    state_machine_arn = os.environ.get('STATE_MACHINE_ARN')
     if not bool(state_machine_arn):
         print("STATE_MACHINE_ARN env variable not set")
-        raise Exception
-    
+        raise Exception("STATE_MACHINE_ARN env variable not set")
+
     response = step_functions_client.start_execution(
         stateMachineArn=state_machine_arn,
     )
 
     if response['ResponseMetadata']['HTTPStatusCode'] != 200:
         print(f"State machine trigger failed with status code: {response['ResponseMetadata']['HTTPStatusCode']}. {response}")
-        raise Exception
+        raise Exception("Failed to start execution")
+
 
 def lambda_handler(event, context):
-    helper(event, context)
+    try:
+        helper(event, context)
+    except Exception as e:
+        print(f"Error in lambda_handler: {str(e)}")
+        helper.init_failure(e)
